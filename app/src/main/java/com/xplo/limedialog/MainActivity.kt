30ca90197539511dd0ff73e3b98480922207a756
xplo <nasif.002@gmail.com>
2020-04-15 15:28:19 +0600

    add example

--- app/src/main/java/com/xplo/limedialog/MainActivity.kt
+++ app/src/main/java/com/xplo/limedialog/MainActivity.kt
@@ -2,11 +2,26 @@ package com.xplo.limedialog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.xlib.limedialog.LimeDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btTest.setOnClickListener {
+
+            LimeDialog.Builder(supportFragmentManager)
+                .setLayoutId(R.layout.custom_bongo_dialog)
+                .setPosButtonText("yes")
+                .setNegButtonText("no")
+                .build()
+                .show()
+
+        }
+
+
     }
 }
